29b929fbc6c1f2795047007b70fddf9af8caef12
matildasutho <64000876+matildasutho@users.noreply.github.com>
2021-09-29 14:42:24 +1000

    Add files via upload

---
+++ troll.js
@@ -0,0 +1,27 @@
+// Note the element ID you would like to insert the troll 
+// either by changing its ID to "trolls"
+// or changing the ID in the src code to match the destination ID
+// The console will become your playground wherein you write
+// a new Digital Folklore of Trolls
+
+
+// Troll 1
+
+var img = document.createElement("img");
+        img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy8AVzxQmvj4qlUJA9WZG_vXF5OQO8pbBP5w&usqp=CAU";
+        var block = document.getElementById("trolls");
+        block.appendChild(img);
+
+// Troll 2
+
+var img = document.createElement("img");
+img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTFQviemN3zQ5KfZoJeLr6SJO2DjTc9YB4Bw&usqp=CAU";
+var block = document.getElementById("trolls");
+block.appendChild(img);
+
+// Troll 2
+
+var img = document.createElement("img");
+img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx-JKc7SpI71x_NRk4Q_g8dspld3SgGcI1vg&usqp=CAU";
+var block = document.getElementById("trolls");
+block.appendChild(img);
